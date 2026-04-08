dd06e81aec03d543f7afe4cc9c8c4f7ed53de93e
ED94120 <denelleeric@gmail.com>
2026-04-08 18:23:01 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,7 @@
 export const APP_CONFIG = {
+  // Important : le chargement automatique des sites analogues suppose au minimum
+  // 5 lignes de PIRE par bande. Une valeur inférieure tronquerait certaines
+  // technologies (ex. BF : 2G900, 3G900, 4G700, 5G700, 4G800).
   nbMaxPireParBande: 5,
   nbDecimalesAffichage: 2,
   modeFFParDefaut: "2bandes",
