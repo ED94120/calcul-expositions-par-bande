605560dcf57b7186d9419266a97844a1c175b04b
ED94120 <denelleeric@gmail.com>
2026-04-08 08:30:37 +0200

    Create sites_pire_analogues.js

---
+++ data/sites_pire_analogues.js
@@ -0,0 +1,55 @@
+const sitesPireAnalogues = [
+  {
+    id: "site_001",
+    operateur: "SFR",
+    environnement: "agglomeration_dense",
+    hma_m: 18,
+    type_document: "DIM",
+    annee: 2024,
+    site_nom: "Nancy - 3 rue Leon Blum",
+    label: "SFR | Agglomération dense | HMA 18 m | DIM 2024 | Nancy - 3 rue Leon Blum",
+    pires: {
+      "2G900": 29,
+      "3G900": 30,
+      "4G700": 33,
+      "4G800": 31,
+      "4G1800": 35,
+      "4G2100": 35,
+      "5G2100": 30,
+      "5G3500": 47
+    }
+  },
+  {
+    id: "site_002",
+    operateur: "SFR",
+    environnement: "zone_montagneuse",
+    hma_m: 23,
+    type_document: "DIM",
+    annee: 2025,
+    site_nom: "Aste",
+    label: "SFR | Zone montagneuse | HMA 23 m | DIM 2025 | Asté",
+    pires: {
+      "3G900": 30,
+      "4G700": 33,
+      "4G800": 33
+    }
+  },
+  {
+    id: "site_003",
+    operateur: "Bouygues",
+    environnement: "zone_rurale",
+    hma_m: 28,
+    type_document: "SIMU",
+    annee: 2025,
+    site_nom: "Nontron - 25 rue des Lillas",
+    label: "Bouygues | Zone rurale | HMA 28 m | SIMU 2025 | Nontron - 25 rue des Lillas",
+    pires: {
+      "3G900": 20,
+      "4G700": 32,
+      "4G800": 30,
+      "4G1800": 33,
+      "4G2600": 35,
+      "5G3500": 40
+    }
+  }
+];
